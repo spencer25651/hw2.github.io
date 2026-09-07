7729501dac8f8f5e422f3554e9d20cfef829b250
Spencer Newman <53012260+spencer25651@users.noreply.github.com>
2026-09-07 14:09:32 -0600

    Add files via upload

---
+++ triangle.js
@@ -0,0 +1,12 @@
+// --- Triangle ---
+const positions = new Float32Array([
+   0.0,  1.0, 0.0,  // vertex 1
+  -1.0, 0.0, 0.0,  // vertex 2
+   1.0, 0.0, 0.0   // vertex 3
+]);
+
+const colors = new Float32Array([
+  1.0, 0.0, 1.0,  // Red
+  0.0, 1.0, 0.0,  // Green
+  0.0, 1.0, 1.0   // Blue
+]);
